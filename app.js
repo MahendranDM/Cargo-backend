1e2340b324fd839aeb35f1d4d4b7018639cd6d2e
VarshaJ004 <varshajohnson333@gmail.com>
2026-07-15 11:00:40 +0530

    Created client AP1

--- app.js
+++ app.js
@@ -7,55 +7,51 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// MongoDB Connection
+mongoose.connect(
+  "mongodb://varsha:toby@ac-yxikmiq-shard-00-00.6mkv1sb.mongodb.net:27017,ac-yxikmiq-shard-00-01.6mkv1sb.mongodb.net:27017,ac-yxikmiq-shard-00-02.6mkv1sb.mongodb.net:27017/shippingservice?ssl=true&replicaSet=atlas-4fm5bt-shard-0&authSource=admin&appName=Cluster0"
+)
+.then(() => console.log("MongoDB Connected"))
+.catch((err) => console.log(err));
 
-mongoose
-  .connect(
-    "mongodb://varsha:toby@ac-yxikmiq-shard-00-00.6mkv1sb.mongodb.net:27017,ac-yxikmiq-shard-00-01.6mkv1sb.mongodb.net:27017,ac-yxikmiq-shard-00-02.6mkv1sb.mongodb.net:27017/shippingservice?ssl=true&replicaSet=atlas-4fm5bt-shard-0&authSource=admin&appName=Cluster0"
-  )
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log(err));
+// Client Company Model
+const Client = mongoose.model(
+  "Client",
+  new mongoose.Schema({
+    companyId: String,
+    companyName: String,
+    contactPerson: String,
+    phoneNumber: String,
+    emailAddress: String,
+    companyAddress: String,
+    city: String,
+    country: String,
+    gstNumber: String,
+    businessType: String
+  })
+);
 
-// Default Route
-app.get("/", (req, res) => {
-  res.send("Shipping Service Management System API");
+// Test API
+app.get("/test", (request, response) => {
+  response.send("Server Working");
 });
 
-// Test Route
-app.get("/test", (req, res) => {
-  res.send("Server Working Successfully");
+// View All Client Companies
+app.post("/view-client", async (request, response) => {
+  const clients = await Client.find();
+  response.json(clients);
 });
 
-/*
-========================================
-TEAM MEMBER 1 - Cargo Management
-========================================
+// Add Client Company
+app.post("/add-client", async (request, response) => {
+  await Client.create(request.body);
 
-// const cargoRoutes = require("./routes/cargo");
-// app.use("/cargo", cargoRoutes);
-
-*/
-
-/*
-========================================
-TEAM MEMBER 2 - Client Management
-========================================
-
-// const clientRoutes = require("./routes/client");
-// app.use("/client", clientRoutes);
-
-*/
-
-/*
-========================================
-TEAM MEMBER 3 - Port Management
-========================================
-
-// const portRoutes = require("./routes/port");
-// app.use("/port", portRoutes);
-
-*/
+  response.json({
+    status: "success"
+  });
+});
 
 // Server
 app.listen(3000, () => {
-  console.log("Server Started on Port 3000");
+  console.log("Server Started");
 });
