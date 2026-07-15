b3ff4dcfacc91d9c44639498cbb467740d2cb2f9
MahendranDM <mahendrandm2003@gmail.com>
2026-07-15 11:09:20 +0530

    Added Port Add API

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ mongoose
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
-// Default Route
 app.get("/", (req, res) => {
   res.send("Shipping Service Management System API");
 });
@@ -25,37 +24,42 @@ app.get("/test", (req, res) => {
   res.send("Server Working Successfully");
 });
 
-/*
-========================================
-TEAM MEMBER 1 - Cargo Management
-========================================
 
-// const cargoRoutes = require("./routes/cargo");
-// app.use("/cargo", cargoRoutes);
+const Port = mongoose.model(
+  "Ports",
+  new mongoose.Schema({
+    port_id: String,
+    port_name: String,
+    port_code: String,
+    city: String,
+    country: String,
+    port_type: String,
+    maximum_cargo_capacity: String,
+    contact_number: String,
+    port_manager: String,
+    operational_status: String,
+  })
+);
 
-*/
+app.post("/api/add-port", async (req, res) => {
+  try {
+    const port = new Port(req.body);
+    const result = await port.save();
 
-/*
-========================================
-TEAM MEMBER 2 - Client Management
-========================================
+    res.status(201).json({
+      status: "success",
+      message: "Port Added Successfully",
+      data: result,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "failed",
+      message: error.message,
+    });
+  }
+});
 
-// const clientRoutes = require("./routes/client");
-// app.use("/client", clientRoutes);
 
-*/
-
-/*
-========================================
-TEAM MEMBER 3 - Port Management
-========================================
-
-// const portRoutes = require("./routes/port");
-// app.use("/port", portRoutes);
-
-*/
-
-// Server
 app.listen(3000, () => {
   console.log("Server Started on Port 3000");
 });
